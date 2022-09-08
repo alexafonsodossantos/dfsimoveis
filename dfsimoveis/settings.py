418077ab33765be686c1478bac2a06b8fd2c4163
alex <aa.santos95@gmail.com>
2022-09-08 17:59:22 -0300

    fixed loading base html

--- dfsimoveis/settings.py
+++ dfsimoveis/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for dfsimoveis project.
 
@@ -14,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -67,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+
 
 WSGI_APPLICATION = 'dfsimoveis.wsgi.application'
 
@@ -122,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
--- imoveis/views.py
+++ imoveis/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from .models import Casa
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    casas = Casa.objects.order_by('data_cadastro')
+    template = loader.get_template('index.html')
+    context = {
+        'casas': casas,
+    }
+    return HttpResponse(template.render(context, request))
