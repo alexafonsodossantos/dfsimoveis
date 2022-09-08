0c63d28a70dfaf2e6ce24dee9e2a75f9aae7a0a4
alex <aa.santos95@gmail.com>
2022-09-08 15:42:54 -0300

    added navbar to base template

--- imoveis/admin.py
+++ imoveis/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from imoveis.models import Casa
 
-# Register your models here.
+admin.site.register(Casa)
--- imoveis/models.py
+++ imoveis/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Casa(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    data_cadastro = models.DateTimeField(default=timezone.now())
+    nome_exibicao = models.CharField(max_length=255, null=True)
+    endereco  = models.CharField(max_length=255, null=True)
+    bairro  = models.CharField(max_length=255, null=True)
+    cidade  = models.CharField(max_length=255, null=True)
+    valor  = models.FloatField()
+    m2_construido = models.IntegerField()
+    m2_total = models.IntegerField()
+    dormitorios = models.IntegerField()
+    suites = models.IntegerField()
+    vagas = models.IntegerField()
+    area_servico = models.BooleanField()
+    churrasqueira = models.BooleanField()
+    piscina = models.BooleanField()
+    area_gourmet = models.BooleanField()
+    descricao = models.TextField()
+    foto_1 = models.ImageField()
+    foto_2 = models.ImageField()
+    foto_3 = models.ImageField()
+    foto_4 = models.ImageField()
+    video = models.ImageField()
