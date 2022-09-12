9f60159f9caa6a0143409db33dd2e70294c5aab2
alexafonsodossantos <aa.santos95@gmail.com>
2022-09-12 13:16:38 -0300

    added detail template and urls, modified detail template

--- dfsimoveis/urls.py
+++ dfsimoveis/urls.py
@@ -7,5 +7,6 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('detalhes/<int:imovel_id>', views.imovel, name='imovel'),
 ]
 urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- imoveis/views.py
+++ imoveis/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Casa
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
 
 def index(request):
@@ -11,3 +12,7 @@ def index(request):
         'casas': casas,
     }
     return HttpResponse(template.render(context, request))
+
+def imovel(request, imovel_id):
+    imovel = get_object_or_404(Casa, pk=imovel_id)
+    return render(request, 'detalhe_imovel.html', {'imovel': imovel})
