2959ef8889ca7d8c9f688faf96aa29939f13cd70
alexafonsodossantos <aa.santos95@gmail.com>
2022-09-16 11:24:39 -0300

    fixed locale and added decimal and hundreth separator

--- dfsimoveis/settings.py
+++ dfsimoveis/settings.py
@@ -110,8 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'pt-br'
+USE_THOUSAND_SEPARATOR = True
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
